3b2dfd6753573d91ecfd5e17fd0a5daabc0ec734
Jawad Saeed <114721996+Jawamegamind@users.noreply.github.com>
2025-07-15 20:49:07 +0500

    Changed to a new navbar

--- card-forge/app/_components/navbar.tsx
+++ card-forge/app/_components/navbar.tsx
@@ -2,33 +2,42 @@ import { logout } from "../login/actions";
 
 export default function Navbar() {
   return (
-    <div className="navbar bg-base-100 shadow-sm">
-        <div className="flex-1">
-            <a className="btn btn-ghost text-xl">CardForge</a>
+    // <!-- Navbar -->
+    <nav className="navbar justify-between bg-base-300">
+        {/* <!-- Logo --> */}
+        <a className="btn btn-ghost text-lg">
+            {/* <img alt="Logo" src="/daisy-components/logo.svg" className="w-4" /> */}
+            CardForge 
+        </a>
+
+        {/* <!-- Menu for mobile --> */}
+        <div className="dropdown dropdown-end sm:hidden">
+            <button className="btn btn-ghost">
+                <i className="fa-solid fa-bars text-lg"></i>
+            </button>
+
+            <ul tabIndex={0} className="dropdown-content menu z-[1] bg-base-200 p-6 rounded-box shadow w-56 gap-2">
+                <li><a>About</a></li>
+                <li><a>Pricing</a></li>
+                <li><a>Blog</a></li>
+                <li><a>Contact</a></li>
+                <a className="btn btn-sm btn-primary">Log in</a>
+            </ul>
         </div>
-        <div className="flex-none">
-            <div className="dropdown dropdown-end">
-                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                    <div className="w-10 rounded-full">
-                    <img
-                        alt="Tailwind CSS Navbar component"
-                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                    </div>
+
+        {/* <!-- Menu for desktop --> */}
+        <ul className="hidden menu sm:menu-horizontal gap-2">
+            <a href="/profile" className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                <img
+                    alt="User profile"
+                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
                 </div>
-                <ul
-                    tabIndex={0}
-                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                    <li>
-                    <a className="justify-between">
-                        Profile
-                        <span className="badge">New</span>
-                    </a>
-                    </li>
-                    <li><a>Settings</a></li>
-                    <li><a onClick={logout}>Logout</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
+            </a>
+            <li><a>Settings</a></li>
+            <a className="btn btn-sm btn-primary" onClick={logout}>Log Out</a>
+        </ul>
+    </nav>
   );
 }
