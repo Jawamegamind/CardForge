60886202aacf0afe5a4b411946956ac32aeaf172
Jawad Saeed <114721996+Jawamegamind@users.noreply.github.com>
2025-07-16 13:28:17 +0500

    Fixed placeholder image rendering

--- card-forge/app/_components/navbar.tsx
+++ card-forge/app/_components/navbar.tsx
@@ -90,12 +90,10 @@ export default function Navbar() {
                             src={userTable.profile_picture_url}
                         />
                     ) : (
-                        <div className="placeholder">
-                            <div className="bg-primary text-primary-content rounded-full w-10 flex items-center justify-center">
-                                <span className="text-sm font-semibold">
-                                    {!loading && user?.email ? user.email.charAt(0).toUpperCase() : 'U'}
-                                </span>
-                            </div>
+                        <div className="bg-base-200 rounded-full w-10 h-10 flex items-center justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-base-content">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                            </svg>
                         </div>
                     )}
                 </div>
